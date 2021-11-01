efc824d412a326a5de1c864b4464acf882e817e9
Ilya Vasilyev <vasilyev.i123@gmail.com>
2021-11-01 22:58:51 +0300

    add input field

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,1 +1,14 @@
 import './input.scss';
+
+import Inputmask from 'inputmask';
+
+$(() => {
+  $('.input[data-mask]').each((index, node) => {
+    const mask = $(node).data('mask');
+    Inputmask({ 
+      mask, 
+      showMaskOnHover: false, 
+      showMaskOnFocus: false 
+    }).mask(node);
+  });
+});
