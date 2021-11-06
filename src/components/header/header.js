f20782b72af9aad5156323bc6a9802c37a001f10
Ilya Vasilyev <vasilyev.i123@gmail.com>
2021-11-06 15:02:02 +0300

    add events for opening the menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,13 @@
 import './header.scss';
+
+import $ from 'jquery';
+
+$(() => {
+  let $body = $('body');
+  let $menu = $('.js-menu');
+
+  $('.js-menu-open').on('click', () => {
+    $body.addClass('menu-opened');
+    $menu.addClass('menu_opened');
+  });
+});
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,1 +1,13 @@
 import './menu.scss';
+
+import $ from 'jquery';
+
+$(() => {
+  let $body = $('body');
+  let $menu = $('.js-menu');
+
+  $('.js-menu-close').on('click', () => {
+    $body.removeClass('menu-opened');
+    $menu.removeClass('menu_opened');
+  });
+});
